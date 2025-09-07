1eecc21d0ebcbdcc3609e474b01f04d24e0b7989
Haider-95 <haider-95@hotmail.com>
2025-09-07 16:22:21 +0200

    börjat med försöka lägga in felmeddelande vid alla och sök delen så man måste söka på hela namnet inputs

--- src/main/java/se/trixzta/s3bucketspj/S3Service.java
+++ src/main/java/se/trixzta/s3bucketspj/S3Service.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 // C3 lägga till så att listobjekten får varsin siffra om tid finnes
-// C4 göra så att det kollar och säger om filen man söker finns efter man valt bucket
+// C4 göra så att det kollar äger om filen man söker finns efter man valt bucket
 @Service
 public class S3Service implements CommandLineRunner {
 
@@ -28,6 +28,7 @@ public class S3Service implements CommandLineRunner {
         Dotenv dotenv = Dotenv.load();
 
         String bucketName = dotenv.get("S3_BUCKET_NAME");
+        String bucketName2 = dotenv.get("S3_BUCKET_NAME2");
         String accessKey = dotenv.get("ACCESS_KEY");
         String secretKey = dotenv.get("SECRET_KEY");
 
@@ -49,18 +50,26 @@ public class S3Service implements CommandLineRunner {
             System.out.println("3.Ladda ner");
             System.out.println("4.Söka bland filer");
             System.out.println("5.Avsluta");
-            System.out.println("Choose an option: ");
-            int choice = Integer.parseInt(scanner.nextLine());
+            System.out.println("6.välj bucket");
+            System.out.println("Choose an option:");
 
-            switch (choice) {
+            String choice = scanner.nextLine();
+            if (!choice.matches("[1-6]")) {
+                System.out.println("Ogiltigt val, försök igen.\n-------\n");
+                continue;
+            }
+            int intChoice = Integer.parseInt(choice);
+
+            switch (intChoice) {
                 case 1:
-                    System.out.println(" Nu listas alla filer! ");
+                    System.out.println("Nu listas alla filer!\n-------\n ");
                     ListObjectsV2Request listReq = ListObjectsV2Request.builder().bucket(bucketName).build();
                     ListObjectsV2Response listRes = s3Client.listObjectsV2(listReq);
                     List<String> listc1 = listRes.contents().stream().map(S3Object::key).collect(Collectors.toList());
                     for (String filnamn : listc1) {
                         System.out.println(filnamn);
                     }
+                    System.out.println("Det var alla filer\n-----\n");
                     break;
 
                 case 2:
@@ -79,24 +88,25 @@ public class S3Service implements CommandLineRunner {
                             .key("S3B/" + keyToUpload).build();
 
                     s3Client.putObject(putReq, RequestBody.fromFile(pathFile.toFile()));
-
-                    PutObjectResponse resp = s3Client.putObject(putReq, RequestBody.fromFile(pathFile.toFile()));
                     System.out.println("Din fil är uppladdad till " + bucketName + "/S3B/" + keyToUpload);
-                    System.out.println("Din Etag är :" + resp.eTag());
+
 
                     break;
                 case 3:
+
 
                     ListObjectsV2Request listReqC3 = ListObjectsV2Request.builder().bucket(bucketName).build();
                     ListObjectsV2Response listResC3 = s3Client.listObjectsV2(listReqC3);
                     List<String> listC3 = listResC3.contents().stream().map(S3Object::key).filter(key -> !key.endsWith("/")).collect(Collectors.toList());
                     for (String filnamn : listC3) {
                         System.out.println(filnamn);
+
                     }
-                    System.out.println("Vilken fil vill du ladda ner?");
+                    System.out.println("\n Vilken fil vill du ladda ner?\n-------\n");
+
                     String downloadFile = scanner.nextLine().trim();
 
-                    boolean Matcher = listC3.contains(downloadFile);
+                    boolean Matcher = listC3.contains("S3B/" + downloadFile);
                     if (!Matcher) {
                         System.out.println("Det du skrivit matchar inte filnamn");
                         break;
@@ -110,7 +120,7 @@ public class S3Service implements CommandLineRunner {
 
                     Path fullpath = downloadPath.resolve(Paths.get(downloadFile).getFileName());
                     if (!fullpath.toFile().exists()) {
-                        System.out.println("filen existerar inte, nedladdning fortsätter");
+                        System.out.println("filen existerar inte på vald adress, nedladdning fortsätter\n-----------\n");
                         break;
                     } else if (fullpath.toFile().exists()) {
                         System.out.println("filen existerar i vald filväg och går inte att ladda ner där");
@@ -124,20 +134,30 @@ public class S3Service implements CommandLineRunner {
                 case 4:
 
                     System.out.println("Vilken fil söker du?");
-                    String searchFile = scanner.nextLine().trim();
-                    String slashFix = searchFile.replace('\\', '/');
+                    String searchFile = scanner.nextLine().trim().toLowerCase();
+                    String FSF = ("S3B/" + searchFile);
+                    ListObjectsV2Response listobjectSF = s3Client.listObjectsV2(ListObjectsV2Request.builder().bucket(bucketName).build());
+                    System.out.println(searchFile);
+                    if (listobjectSF.contents().stream().anyMatch(obj -> obj.key().matches(FSF))) {
+                        System.out.println("Filen " + FSF + " finns i din bucket\n-------\n");
+                        System.out.println(searchFile);
+                    } else {
+                        System.out.println("Filen " + FSF + " finns inte i din bucket\n-------\n");
+                        System.out.println(searchFile);
 
-
+                    }
+//lägga till så man kan söka med del av filnamn
 
                     break;
 
 
-
                 case 5:
                     return;
-                default:
-            }
 
+                default:
+
+            }
         }
+
     }
 }
