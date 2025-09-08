f2d541d74a80b18adc092f30e1e9819aa4a2e24b
haider <Haider-95@hotmail.com>
2025-09-08 12:51:54 +0200

    gjort case för att söka filer men bara helnamn, och  för att ta bort en fil från bucket. samt lagt till mer felmeddelanden. lagt in en huvudmeny där man väljer bucket och sen får man alternativen och håller på med att fixa så man kan ladda upp zip

--- src/main/java/se/trixzta/s3bucketspj/S3Service.java
+++ src/main/java/se/trixzta/s3bucketspj/S3Service.java
@@ -10,21 +10,25 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 
+import javax.xml.crypto.dsig.Transform;
+import java.io.File;
+import java.lang.ref.ReferenceQueue;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+import static software.amazon.awssdk.services.s3.model.ObjectIdentifier.*;
+
 // C3 lägga till så att listobjekten får varsin siffra om tid finnes
 // C4 göra så att det kollar och säger om filen man söker finns efter man valt bucket
 @Service
 public class S3Service implements CommandLineRunner {
 
-
     @Override
-    public void run(String... args) throws Exception {
-
+    public void run(String... args) {
 
         Dotenv dotenv = Dotenv.load();
 
@@ -44,118 +48,181 @@ public class S3Service implements CommandLineRunner {
                 .region(Region.EU_NORTH_1)
                 .build();
 
-
         while (true) {
-            System.out.println("1.Lista alla filer");
-            System.out.println("2.Ladda upp fil");
-            System.out.println("3.Ladda ner");
-            System.out.println("4.Söka bland filer");
-            System.out.println("5.Avsluta");
-            System.out.println("6.välj bucket");
-            System.out.println("Choose an option:");
 
-            String choice = scanner.nextLine();
-            if (!choice.matches("[1-6]")) {
-                System.out.println("Ogiltigt val, försök igen.\n-------\n");
-                continue;
+            System.out.println("\nVälkommen till S3 Bucket hanteraren\n");
+            System.out.println("Vilken bucket vill du använda ? ");
+            System.out.println("1. " + bucketName);
+            System.out.println("2. " + bucketName2);
+            System.out.println("3. Avsluta\n");
+            String bucketChoice = scanner.nextLine();
+
+
+            if (bucketChoice.equals("1")) {
+                bucketChoice = bucketName;
+            } else if (bucketChoice.equals("2")) {
+                bucketChoice = bucketName2;
+            } else if (bucketChoice.equals("3")) {
+                System.out.println("avsluta");
+            } else {
+                System.out.println("Ogiltigt val, avslutar programmet");
+                return;
             }
-            int intChoice = Integer.parseInt(choice);
 
-            switch (intChoice) {
-                case 1:
-                    System.out.println("Nu listas alla filer!\n-------\n ");
-                    ListObjectsV2Request listReq = ListObjectsV2Request.builder().bucket(bucketName).build();
-                    ListObjectsV2Response listRes = s3Client.listObjectsV2(listReq);
-                    List<String> listc1 = listRes.contents().stream().map(S3Object::key).collect(Collectors.toList());
-                    for (String filnamn : listc1) {
-                        System.out.println(filnamn);
-                    }
-                    System.out.println("Det var alla filer\n-----\n");
-                    break;
 
-                case 2:
-                    System.out.println("Vilken fil vill du ladda upp ? ");
-                    Path pathFile = Paths.get(scanner.nextLine());
+            while (bucketChoice == "1" || bucketChoice == "2" || !bucketChoice.isEmpty()) {
+                System.out.println("\n1.Lista alla filer");
+                System.out.println("2.Ladda upp fil");
+                System.out.println("3.Ladda ner");
+                System.out.println("4.Söka bland filer");
+                System.out.println("5.Ta bort filer");
+                System.out.println("6.Ladda upp mapp som zip");
+                System.out.println("Välj ett alternativ:\n");
 
-                    if (!pathFile.toFile().exists()) {
-                        System.out.println("filen du har valt existerar inte, du går tillbaka tll menyn ");
+                String choice = scanner.nextLine();
+                if (!choice.matches("[1-6]")) {
+                    System.out.println("Ogiltigt val, försök igen.\n-------\n");
+                    continue;
+                }
+                int intChoice = Integer.parseInt(choice);
+
+                switch (intChoice) {
+                    case 1:
+                        System.out.println("\nNu listas alla filer!\n-------\n ");
+                        ListObjectsV2Request listReq = ListObjectsV2Request.builder().bucket(bucketChoice).build();
+                        ListObjectsV2Response listRes = s3Client.listObjectsV2(listReq);
+                        List<String> listc1 = listRes.contents().stream().map(S3Object::key).collect(Collectors.toList());
+                        for (String filnamn : listc1) {
+                            System.out.println(filnamn);
+                        }
+                        System.out.println("\n-----Det var alla filer\n");
                         break;
-                    }
-                    System.out.println("Vad ska filen heta i bucket ? ");
-                    String keyToUpload = scanner.nextLine().trim().toLowerCase();
 
-                    PutObjectRequest putReq = PutObjectRequest.builder()
-                            .bucket(bucketName)
-                            .key("S3B/" + keyToUpload).build();
+                    case 2:
+                        System.out.println("Vilken fil vill du ladda upp ? ");
+                        Path pathFile = Paths.get(scanner.nextLine());
 
-                    s3Client.putObject(putReq, RequestBody.fromFile(pathFile.toFile()));
-                    System.out.println("Din fil är uppladdad till " + bucketName + "/S3B/" + keyToUpload);
+                        if (!pathFile.toFile().exists()) {
+                            System.out.println("filen du har valt existerar inte, du går tillbaka tll menyn ");
+                            break;
+                        }
+                        System.out.println("Vad ska filen heta i bucket ? ");
+                        String keyToUpload = scanner.nextLine().trim().toLowerCase();
+
+                        PutObjectRequest putReq = PutObjectRequest.builder()
+                                .bucket(bucketChoice)
+                                .key(keyToUpload).build();
+
+                        s3Client.putObject(putReq, RequestBody.fromFile(pathFile.toFile()));
+                        System.out.println("Din fil är uppladdad till " + bucketChoice + "\\" + keyToUpload);
 
 
-                    break;
-                case 3:
-
-                    ListObjectsV2Request listReqC3 = ListObjectsV2Request.builder().bucket(bucketName).build();
-                    ListObjectsV2Response listResC3 = s3Client.listObjectsV2(listReqC3);
-                    List<String> listC3 = listResC3.contents().stream().map(S3Object::key).filter(key -> !key.endsWith("/")).collect(Collectors.toList());
-                    for (String filnamn : listC3) {
-                        System.out.println(filnamn);
-                    }
-                    System.out.println("\n Vilken fil vill du ladda ner?\n-------\n");
-
-                    String downloadFile = scanner.nextLine().trim();
-
-                    boolean Matcher = listC3.contains("S3B/" + downloadFile);
-                    if (!Matcher) {
-                        System.out.println("Det du skrivit matchar inte filnamn");
                         break;
-                    }
-                    System.out.println("Vart vill du spara filen ? ");
-                    Path downloadPath = Paths.get(scanner.nextLine());
-                    if (!downloadPath.toFile().exists()) {
-                        System.out.println("Filvägen existerar inte");
-                        break;
-                    }
+                    case 3:
 
-                    Path fullpath = downloadPath.resolve(Paths.get(downloadFile).getFileName());
-                    if (!fullpath.toFile().exists()) {
-                        System.out.println("filen existerar inte på vald adress, nedladdning fortsätter\n-----------\n");
-                        break;
-                    } else if (fullpath.toFile().exists()) {
-                        System.out.println("filen existerar i vald filväg och går inte att ladda ner där");
-                        break;
-                    }
-                    s3Client.getObject(request -> request
-                                    .bucket(bucketName)
-                                    .key(downloadFile),
-                            ResponseTransformer.toFile(fullpath.toFile()));
-                    break;
-                case 4:
+                        ListObjectsV2Request listReqC3 = ListObjectsV2Request.builder().bucket(bucketChoice).build();
+                        ListObjectsV2Response listResC3 = s3Client.listObjectsV2(listReqC3);
+                        List<String> listC3 = listResC3.contents().stream().map(S3Object::key).filter(key -> !key.endsWith("/")).collect(Collectors.toList());
+                        for (String filnamn : listC3) {
+                            System.out.println(filnamn);
+                        }
+                        System.out.println("\n Vilken fil vill du ladda ner?\n-------\n");
 
-                    System.out.println("Vilken fil söker du?");
-                    String searchFile = scanner.nextLine().trim().toLowerCase();
-                    String FSF = ("S3B/" + searchFile);
-                    ListObjectsV2Response listobjectSF = s3Client.listObjectsV2(ListObjectsV2Request.builder().bucket(bucketName).build());
-                    System.out.println(searchFile);
-                    if (listobjectSF.contents().stream().anyMatch(obj -> obj.key().matches(FSF))) {
-                        System.out.println("Filen " + FSF + " finns i din bucket\n-------\n");
-                        System.out.println(searchFile);
-                    } else {
-                        System.out.println("Filen " + FSF + " finns inte i din bucket\n-------\n");
-                        System.out.println(searchFile);
+                        String downloadFile = scanner.nextLine().trim();
 
-                    }
+                        boolean Matcher = listC3.contains(downloadFile);
+                        if (!Matcher) {
+                            System.out.println("Det du skrivit matchar inte filnamn");
+                            break;
+                        }
+                        System.out.println("Vart vill du spara filen ? ");
+                        Path downloadPath = Paths.get(scanner.nextLine());
+                        if (!downloadPath.toFile().exists()) {
+                            System.out.println("Filvägen existerar inte");
+                            break;
+                        }
+
+                        Path fullpath = downloadPath.resolve(Paths.get(downloadFile).getFileName());
+                        if (!fullpath.toFile().exists()) {
+                            System.out.println("\nFilen existerar inte på vald adress, nedladdning fortsätter\n-----------\n");
+                            break;
+                        } else if (fullpath.toFile().exists()) {
+                            System.out.println("\nFilen existerar i vald filväg och går inte att ladda ner där");
+                            break;
+                        }
+                        s3Client.getObject(request -> request
+                                        .bucket(bucketName)
+                                        .key(downloadFile),
+                                ResponseTransformer.toFile(fullpath.toFile()));
+                        break;
+                    case 4:
+
+                        System.out.println("Vilken fil söker du?");
+                        String searchFile = scanner.nextLine().trim().toLowerCase();
+                        String FSF = (searchFile);
+                        ListObjectsV2Response listobjectSF = s3Client.listObjectsV2(ListObjectsV2Request.builder().bucket(bucketName).build());
+
+                        if (listobjectSF.contents().stream().anyMatch(obj -> obj.key().matches(FSF))) {
+                            System.out.println(FSF + " Finns i din bucket\n-------\n");
+                        } else {
+                            System.out.println(FSF + " Finns inte i din bucket\n-------\n");
+
+
+                        }
 //lägga till så man kan söka med del av filnamn
 
-                    break;
+
+                        break;
+                    case 5:
+                        System.out.println("Här får du en lista med filer.\n-------\n");
+                        ListObjectsV2Request listReqC6 = ListObjectsV2Request.builder().bucket(bucketChoice).build();
+                        ListObjectsV2Response listResC6 = s3Client.listObjectsV2(listReqC6);
+                        List<String> listC5 = listResC6.contents().stream().map(S3Object::key).collect(Collectors.toList());
+                        for (String filnamn : listC5) {
+                            System.out.println(filnamn);
+                        }
+                        System.out.println("Vilken fil vill du radera \n-------\n");
+                        String deleteChoice = scanner.nextLine().trim();
 
 
+                        DeleteObjectResponse deleteObjectsRes = s3Client.deleteObject(DeleteObjectRequest.builder().bucket(bucketChoice).key(deleteChoice).build());
+                        if (listC5.contains(deleteChoice)) {
+                            System.out.println("Filen " + deleteChoice + " finns inte din bucket längre");
+                            break;
 
-                case 5:
-                    return;
-                default:
+                        } else {
+                            System.out.println("Fel input\n");
+                        }
+                        break;
+                    case 6:
+                        System.out.println("Vilken mapp vill du ladda upp");
+                        Path folderPath = Paths.get(scanner.nextLine());
+                        if (Files.notExists(folderPath) || !Files.isDirectory(folderPath)) {
+                            System.out.println("mappen du har valt existerar inte, du går tillbaka tll menyn ");
+                            break;
+                        }else {      System.out.println(folderPath + " existerar  ");
+                        }
+
+                        System.out.println("Vad ska den heta? ");
+                        String zipNaming = scanner.nextLine().trim().toLowerCase();
+
+                        if (zipNaming.length() == 0) {
+                            System.out.println("ingen input");
+                            break;
+                        }
+
+                        PutObjectRequest putReqZip = PutObjectRequest.builder()
+                                .bucket(bucketChoice)
+                                .key(zipNaming).build();
+
+                        PutObjectResponse putObjectResponse;
+
+                        System.out.println("Din fil är uppladdad till " + bucketChoice + "\\" + zipNaming);
+                        break;
+
+                }
+                break;
             }
-
         }
     }
 }
